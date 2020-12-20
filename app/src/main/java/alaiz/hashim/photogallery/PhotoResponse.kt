4c886a8e9ba675e0d61aaca84144bfe15f314bb3
Al-Aiz Hashim <AlAiz.Hashim@gmail.com>
2020-12-20 22:59:01 +0300

    Adding PhotoResponse

---
+++ app/src/main/java/alaiz/hashim/photogallery/PhotoResponse.kt
@@ -0,0 +1,8 @@
+package alaiz.hashim.photogallery
+
+import com.google.gson.annotations.SerializedName
+
+class PhotoResponse {
+    @SerializedName("photo")
+    lateinit var galleryItems: List<GalleryItem>
+}
