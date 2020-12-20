93540a0f88f85851ea5f25e9ad034f7c58940fec
Al-Aiz Hashim <AlAiz.Hashim@gmail.com>
2020-12-20 22:30:20 +0300

    Using the Retrofit object to create an instance of the API

--- app/src/main/java/alaiz/hashim/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/alaiz/hashim/photogallery/PhotoGalleryFragment.kt
@@ -1,5 +1,6 @@
 package alaiz.hashim.photogallery
 
+import alaiz.hashim.photogallery.api.FlickrApi
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,13 +8,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Retrofit
 
 
 class PhotoGalleryFragment : Fragment() {
     private lateinit var photoRecyclerView: RecyclerView
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://www.flickr.com/")
+            .build()
+        val flickrApi: FlickrApi = retrofit.create(FlickrApi::class.java)
 
     }
 
