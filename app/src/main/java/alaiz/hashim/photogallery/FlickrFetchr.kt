780e04c0d16c16ccd9e744704459ef76bd33cb98
Al-Aiz Hashim <AlAiz.Hashim@gmail.com>
2020-12-20 22:49:56 +0300

    Using FlickrFetchr in PhotoGalleryFragment

--- app/src/main/java/alaiz/hashim/photogallery/FlickrFetchr.kt
+++ app/src/main/java/alaiz/hashim/photogallery/FlickrFetchr.kt
@@ -1,6 +1,12 @@
 package alaiz.hashim.photogallery
 
 import alaiz.hashim.photogallery.api.FlickrApi
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
@@ -14,4 +20,21 @@ class FlickrFetchr {
             .build()
         flickrApi = retrofit.create(FlickrApi::class.java)
     }
+    fun fetchContents(): LiveData<String> {
+        val responseLiveData: MutableLiveData<String> = MutableLiveData()
+        val flickrRequest: Call<String> = flickrApi.fetchContents()
+        flickrRequest.enqueue(object : Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(TAG, "Failed to fetch photos", t)
+            }
+            override fun onResponse(
+                call: Call<String>,
+                response: Response<String>
+            ) {
+                Log.d(TAG, "Response received")
+                responseLiveData.value = response.body()
+            }
+        })return responseLiveData
+    }
+}
 }
