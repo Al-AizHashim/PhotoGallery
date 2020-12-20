9f415dcf00e0a4a8cdc40bbe86f963410ed6bc66
Al-Aiz Hashim <AlAiz.Hashim@gmail.com>
2020-12-20 22:23:05 +0300

    Setting up the fragment

--- app/src/main/java/alaiz/hashim/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/alaiz/hashim/photogallery/PhotoGalleryFragment.kt
@@ -5,12 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 class PhotoGalleryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var photoRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +22,10 @@ class PhotoGalleryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_photo_gallery, container, false)
+        val view = inflater.inflate(R.layout.fragment_photo_gallery, container, false)
+        photoRecyclerView = view.findViewById(R.id.photo_recycler_view)
+        photoRecyclerView.layoutManager = GridLayoutManager(context, 3)
+        return view
     }
 
     companion object {
@@ -34,3 +37,6 @@ class PhotoGalleryFragment : Fragment() {
 
     }
 }
+
+
+
